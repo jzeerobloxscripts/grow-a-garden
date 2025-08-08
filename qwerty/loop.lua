ee98524971d7a8ea08142c2bf3c891d6fdafbf94
RealJzee <jzeeroblox@gmail.com>
2025-08-08 18:18:49 +0800

    Update loop.lua

--- qwerty/loop.lua
+++ qwerty/loop.lua
@@ -1,5 +1,5 @@
 queue_on_teleport([[
 loadstring(game:HttpGet('https://raw.githubusercontent.com/jzeerobloxscripts/grow-a-garden/refs/heads/main/qwerty/loop.lua'))()
 ]])
-wait(5)
+wait(50)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/jzeerobloxscripts/grow-a-garden/refs/heads/main/qwerty/generalfunctions.lua"))()
